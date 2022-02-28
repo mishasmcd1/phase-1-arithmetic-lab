1dbfe7bf60f4400e504db9c1ce96d1dbed7d71a4
mishasmcd1 <msm1@uchicago.edu>
2022-02-27 18:12:08 -0600

    commit tree 765f28a2552be1a7b6f88efd95b43b994f5cb6ea

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+const random = Math.round(Math.random()*1000 + 1);
+let num3 = 12;
+let num4 = 8;
+let mod = num3 % num4;
+let max = Math.max(7, 14, 11, 20);
